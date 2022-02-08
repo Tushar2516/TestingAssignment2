5acb0678c8962177351d46d008123425f3bddb31
tushar patel <pateltushar4242@gmail.com>
2022-02-08 01:45:51 -0500

    Add project files.

---
+++ TestingAssignment2/Conversion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingAssignment2
+{
+        public class Conversion
+    {
+        private int ConversionNumber;
+        private double GetConvertedResult;
+
+        public Conversion()
+        {
+            this.ConversionNumber = 1;
+        }
+
+        public Conversion(int ConversionNumber)
+        {
+            this.ConversionNumber = ConversionNumber;
+        }
+
+        public double ConvertCelciusToFahrenheit()
+        {
+
+            GetConvertedResult = ((ConversionNumber * 9) / 5) + 32;
+            return GetConvertedResult;
+        }
+
+        public double ConvertCelciusToKelvin()
+        {
+
+            GetConvertedResult = (ConversionNumber + 273.15);
+            return GetConvertedResult;
+        }
+
+
+        public double ConvertKelvinToFahrenheit()
+        {
+            GetConvertedResult = ((ConversionNumber - 273.15) * 1.8 + 32);
+            return GetConvertedResult;
+        }
+
+        public double ConvertKelvinToCelcuis()
+        {
+
+            GetConvertedResult = (ConversionNumber - 273.15);
+            return GetConvertedResult;
+        }
+
+
+        public double ConvertFahrenheitToKelvin()
+        {
+
+            GetConvertedResult = ((ConversionNumber - 32) / 1.8) + 273.15;
+            return GetConvertedResult;
+        }
+
+        public double ConvertFahrenheitToCelcius()
+        {
+
+            GetConvertedResult = ((ConversionNumber - 32) / 9) * 5;
+            return GetConvertedResult;
+        }
+
+    }
+
+}
